b8195d859b267e02bf1e0ded03f04c02333a5a18
Adolfo Esparza <esparzasordoadolfo@gmail.com>
2023-10-22 20:08:35 -0600

    disabling sslmode

--- server.py
+++ server.py
@@ -35,7 +35,7 @@ class DBContext:
                 user=DATABASE_CONFIGS['user'],
                 password=DATABASE_CONFIGS['password'],
                 port=DATABASE_CONFIGS['port'],
-                sslmode='require'
+                sslmode='disable'
             )
         else:
             self.con = sqlite3.connect(DATABASE_DEFAULT)
