fef517a9df60dc8cf843f94dde75b545bcbfb950
Adolfo Esparza <esparzasordoadolfo@gmail.com>
2023-10-22 19:44:45 -0600

    require sslmode for psycopg2

--- server.py
+++ server.py
@@ -34,7 +34,8 @@ class DBContext:
                 host=DATABASE_CONFIGS['host'],
                 user=DATABASE_CONFIGS['user'],
                 password=DATABASE_CONFIGS['password'],
-                port=DATABASE_CONFIGS['port']
+                port=DATABASE_CONFIGS['port'],
+                sslmode='require'
             )
         else:
             self.con = sqlite3.connect(DATABASE_DEFAULT)
